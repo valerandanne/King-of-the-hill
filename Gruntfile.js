624f877dfde531317e354367a53e14ed0710db3d
valerandanne <valentinarandanne@gmail.com>
2015-07-08 10:30:40 -0300

    ..

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     
-    grunt.registerTask('default',['jshint']);
-    
+    grunt.registerTask('default',['watch']);
+    grunt.registerTask('build', ['jshint']);
 };
